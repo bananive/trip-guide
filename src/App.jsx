341fbd68333952b90d512abe4a08e1dcfb6d5ac1
bananive <fr8ntend@gmail.com>
2021-11-23 08:23:21 +0500

    adding router to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+
 const App = () => {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
